dec8d1eb044264af6babf0dcde4b448bb1903300
Sophie <sophielin14@gmail.com>
2023-10-31 19:21:34 -0700

    part1 done

--- lab6/lab6_part1.py
+++ lab6/lab6_part1.py
@@ -8,7 +8,7 @@ import multiprocessing
 import random #is used to cause some randomness 
 import time   #is used to cause some delay to simulate thinking or eating times
 
-def philosopher(id: int, chopstick: list): 
+def philosopher(id: int, chopstick: list, atTable): 
     """
        implements a thinking-eating philosopher
        id is used to identifier philosopher #id (id is between 0 to numberOfPhilosophers-1)
@@ -22,10 +22,7 @@ def philosopher(id: int, chopstick: list):
         print(f"DEBUG: philosopher{id} thinking")
         time.sleep(round(random.uniform(.1, .3), 2)) #a random delay (100 to 300 ms)
 
-    # part 1 - only allow 4 philosophers at the table
-    # approach - don't let the last philosopher sit at the table
-    if (id == 5 - 1):      #5 is number of philosophers
-        return
+    atTable.acquire() # acquire (decrement) part 1 semaphore
 
     for _ in range(6): #to make testing easier, instead of a forever loop we use a finite loop
         leftChopstick = id
@@ -46,6 +43,8 @@ def philosopher(id: int, chopstick: list):
 
         thinkForAWhile()  #use this line as is
 
+    atTable.release() # release (increment) part 1 semaphore
+
 if __name__ == "__main__":
     semaphoreList = list()          #this list will hold one semaphore per chopstick
     numberOfPhilosophers = 5
@@ -53,9 +52,13 @@ if __name__ == "__main__":
     for i in range(numberOfPhilosophers):             
         semaphoreList.append(multiprocessing.Semaphore(1))    #one semaphore per chopstick
 
+    # semaphore for part 1 - number of philosophers at the table
+    #                        only allow 4
+    atTable = multiprocessing.Semaphore(numberOfPhilosophers - 1)
+
     philosopherProcessList = list()
     for i in range(numberOfPhilosophers): #instantiate all processes representing philosophers
-        philosopherProcessList.append(multiprocessing.Process(target=philosopher, args=(i, semaphoreList)))
+        philosopherProcessList.append(multiprocessing.Process(target=philosopher, args=(i, semaphoreList, atTable)))
     for j in range(numberOfPhilosophers): #start all child processes
         philosopherProcessList[j].start()
     for k in range(numberOfPhilosophers): #join all child processes
